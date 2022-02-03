d4de78ca2e313854e7293822f15d521d4eb76989
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2022-02-03 12:41:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-function preload(){
-}
+noseX=0;
+noseY=0;
 
+
+function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
 function setup() {
     canvas = createCanvas(500,400);
     canvas.position(70,200);
     video = createCapture(VIDEO);
+    video.size(500,400);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video,100,80,300,250);
-
+    image(mustache, noseX-32, noseY-23, 50, 30);
     stroke(168, 10, 10);
     fill(168, 10, 10);
     
@@ -31,4 +39,18 @@ function draw() {
 
 function take_snapshot(){
     save("frame.png");
+}
+
+function modelLoaded(){
+    console.log('PoseNet Id Initialized');
+}
+
+function gotPoses(results){
+    if(results.length> 0){
+        console.log(results);
+        console.log("nose x ="+ noseX);
+        console.log("nose y ="+ noseY);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
 }
